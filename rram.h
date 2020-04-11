10f438cfb91469db376057e8d87e1bbdd1f65045
yx-lu <55081917+yx-lu@users.noreply.github.com>
2020-04-11 18:08:19 +0800

    初步完成

--- rram.h
+++ rram.h
@@ -2,11 +2,13 @@
 #include<bitset>
 #include<cassert>
 const int RRAM_SIZE=512;
-int INPUT_CPU_FROM_RRAM,OUTPUT_CPU_TO_RRAM,RRAM_OPERATION_MAGIC,RRAM_OPERATION_MATRIX_VECTOR_MULT,RRAM_CROSS_REGION_TRANSFER;
+int INPUT_CPU_FROM_RRAM,OUTPUT_CPU_TO_RRAM;
+int RRAM_PARALLEL_POSITION_SET,RRAM_OPERATION_MAGIC,RRAM_OPERATION_MATRIX_VECTOR_MULT,RRAM_CROSS_REGION_TRANSFER;
 int RRAM_USED,RRAM_MAXUSED;
 typedef std::bitset<RRAM_SIZE> BIT;
 #define Input INPUT_CPU_FROM_RRAM
 #define Output OUTPUT_CPU_TO_RRAM
+#define Posset RRAM_PARALLEL_POSITION_SET
 #define Magic RRAM_OPERATION_MAGIC
 #define Mult RRAM_OPERATION_MATRIX_VECTOR_MULT
 #define Trans RRAM_CROSS_REGION_TRANSFER
@@ -15,22 +17,26 @@ typedef std::bitset<RRAM_SIZE> BIT;
 #define Size RRAM_SIZE
 struct RRAM
 {
-	BIT data[Size],x,y;
+	BIT data[Size],x,y,lp,cp;
 	//x:input buffer
 	//y:output buffer
 	//data:data
+	//lp:line parallel positions
+	//cp:column parallel positions
 	
-
+	
 	//initialization
-	RRAM()
+	RRAM()//init=all 1
 	{
-		Use++;if (Maxuse<Use) Maxuse=Use;x.reset();y.reset();
-		for (int i=0;i<Size;i++) data[i].reset();
+		Use++;if (Maxuse<Use) Maxuse=Use;x.set();y.set();
+		for (int i=0;i<Size;i++) data[i].set();
+		lp.set();cp.set();
 	}
 	RRAM(BIT init[Size])
 	{
-		Use++;if (Maxuse<Use) Maxuse=Use;x.reset();y.reset();
+		Use++;if (Maxuse<Use) Maxuse=Use;x.set();y.set();
 		for (int i=0;i<Size;i++) data[i]=init[i];
+		lp.set();cp.set();
 	}
 	~RRAM()
 	{
@@ -38,51 +44,72 @@ struct RRAM
 	}
 	
 	
+	//set operation
+	void changelp(BIT b){Posset++;lp=b;}
+	void changecp(BIT b){Posset++;cp=b;}
+	void lineset(int i){for (int k=0;k<Size;k++) if (lp[k]) data[i][k]=1;}
+	void linereset(int i){for (int k=0;k<Size;k++) if (lp[k]) data[i][k]=0;}
+	void columnset(int i){for (int k=0;k<Size;k++) if (lp[k]) data[k][i]=1;}
+	void columnreset(int i){for (int k=0;k<Size;k++) if (lp[k]) data[k][i]=0;}
+	
+	
 	//basic calculation
-	void lineop(int i,int j)//linei &= ! linej
+	void lineop(int i,int j)//li&=!lj
 	{
 		if ((i<0)||(i>=Size)||(j<0)||(j>=Size)||(i==j)) {fprintf(stderr,"Index overflow!");assert(0);}
 		Magic++;
-		for (int k=0;k<Size;k++) data[i][k]=data[i][k]&(!data[j][k]);
+		for (int k=0;k<Size;k++) if (lp[k]) data[i][k]=data[i][k]&(!data[j][k]);
 	}
-	void columnop(int i,int j)//columni &= ! columnj
+	void lineop(int i,int j1,int j2)//li&=(!lj1)&(!lj2)
+	{
+		if ((i<0)||(i>=Size)||(j1<0)||(j1>=Size)||(j2<0)||(j2>Size)||(i==j1)||(i==j2)) {fprintf(stderr,"Index overflow!");assert(0);}
+		Magic++;
+		for (int k=0;k<Size;k++) if (lp[k]) data[i][k]=data[i][k]&(!data[j1][k])&(!data[j2][k]);
+	}
+	void columnop(int i,int j)//ci&=!cj
 	{
 		if ((i<0)||(i>=Size)||(j<0)||(j>=Size)||(i==j)) {fprintf(stderr,"Index overflow!");assert(0);}
 		Magic++;
-		for (int k=0;k<Size;k++) data[k][i]=data[k][i]&(!data[k][j]);
+		for (int k=0;k<Size;k++) if (cp[k]) data[k][i]=data[k][i]&(!data[k][j]);
 	}
-	void mult()//calculate this.y=this.x * this.data (and mult)
+	void columnop(int i,int j1,int j2)//ci&=(!cj1)&(!cj2)
+	{
+		if ((i<0)||(i>=Size)||(j1<0)||(j1>=Size)||(j2<0)||(j2>Size)||(i==j1)||(i==j2)) {fprintf(stderr,"Index overflow!");assert(0);}
+		Magic++;
+		for (int k=0;k<Size;k++) if (cp[k]) data[k][i]=data[k][i]&(!data[k][j1])&(!data[k][j2]);
+	}
+	void mult()//y=data*x (and mult)
 	{
 		Mult++;
 		y.reset();
-		for (int i=0;i<Size;i++) y|=x&data[i];
+		for (int i=0;i<Size;i++) y[i]=(x&data[i]).any();
 	}
 	
 	
 	//basic buffer operation
 	void line2buf(int i)//output line_i to y
 	{
-		Magic++;//indead it needs more than 1 Magicop
+		Magic+=2;
 		y=data[i];
 	}
 	void column2buf(int i)//output column_i to y
 	{
-		Magic++;
+		Magic+=2;
 		for (int k=0;k<Size;k++) y[k]=data[k][i];
 	}
 	void buf2line(int i)//input line_i from x
 	{
-		Magic++;
+		Magic+=2;
 		data[i]=x;
 	}
 	void buf2column(int i)//input column_i from x
 	{
-		Magic++;
+		Magic+=2;
 		for (int k=0;k<Size;k++) data[k][i]=x[k];
 	}
 	void buf2buf()//y=x
 	{
-		Magic++;
+		Magic+=2;
 		y=x;
 	}
 	
@@ -159,6 +186,7 @@ void Printinfo(FILE* file=stderr)//run this before ending
 {
 	fprintf(file,"RRAM_USED: %d\n",RRAM_USED);
 	fprintf(file,"RRAM_MAXUSED: %d\n",RRAM_MAXUSED);
+	fprintf(file,"RRAM_PARALLEL_POSITION_SET: %d\n",RRAM_PARALLEL_POSITION_SET);
 	fprintf(file,"INPUT_CPU_FROM_RRAM: %d\n",INPUT_CPU_FROM_RRAM);
 	fprintf(file,"OUTPUT_CPU_TO_RRAM: %d\n",OUTPUT_CPU_TO_RRAM);
 	fprintf(file,"RRAM_OPERATION_MAGIC: %d\n",RRAM_OPERATION_MAGIC);
---
+++ rram_allocator.h
@@ -0,0 +1,135 @@
+#include"rram.h"
+#include<vector>
+const int MAX_USED=RRAM_SIZE-4;
+std::vector<RRAM*> pool;
+int lineused=MAX_USED;
+struct row
+{
+	RRAM *fr;
+	int fore,back;
+	
+	
+	//init and copy
+	row(int v=0)
+	{
+		if (lineused==MAX_USED) {RRAM* nw=new RRAM;pool.push_back(nw);lineused=0;}
+		fr=pool.back();
+		fore=lineused++;
+		back=lineused++;
+		if (v==1) fr->linereset(back);
+		else if (v==0) fr->linereset(fore);
+		else if (v==-1) {}
+		else assert(0);
+	}
+	row(const row &y)
+	{
+		fr=y.fr;
+		fore=y.fore;
+		back=y.back;
+	}
+	friend row operator !(const row &x)
+	{
+		row y(x);
+		std::swap(y.fore,y.back);
+		return y;
+	}
+	friend row deepcopy(const row &y)
+	{
+		row x(0);
+		x|=y;
+		return x;
+	}
+	
+	
+	//set and reset
+	void set()
+	{
+		fr->lineset(fore);
+		fr->linereset(back);
+	}
+	void reset()
+	{
+		fr->linereset(fore);
+		fr->lineset(back);
+	}
+	
+	//read and write
+	void write(BIT b)
+	{
+		fr->writeline(b,fore);
+		fr->lineset(back);
+		fr->lineop(back,fore);
+	}
+	BIT read(){return fr->readline(fore);}
+	
+	
+	//calculation
+	void operator |=(const row &y)
+	{
+		if (y.fr==fr) fr->lineop(back,y.fore);
+		else
+		{
+			transll(*y.fr,y.fore,*fr,RRAM_SIZE-1);
+			fr->lineop(back,RRAM_SIZE-1);
+		}
+		fr->lineset(fore);
+		fr->lineop(fore,back);
+		//nx&=!y
+		//x=1
+		//x&=!nx
+	}
+	void operator &=(const row &y)
+	{
+		if (y.fr==fr) fr->lineop(fore,y.back);
+		else
+		{
+			transll(*y.fr,y.back,*fr,RRAM_SIZE-1);
+			fr->lineop(fore,RRAM_SIZE-1);
+		}
+		fr->lineset(back);
+		fr->lineop(back,fore);
+		//x&=!ny
+		//nx=1
+		//nx&=!x
+	}
+	void operator ^=(const row &y)
+	{
+		int yfore,yback;
+		if (y.fr==fr) {yfore=y.fore;yback=y.back;}
+		else
+		{
+			transll(*y.fr,y.fore,*fr,RRAM_SIZE-1);
+			yfore=RRAM_SIZE-1;yback=RRAM_SIZE-2;
+			fr->lineset(yback);
+			fr->lineop(back,fore);
+		}
+		int z=RRAM_SIZE-3,nz=RRAM_SIZE-4,w=RRAM_SIZE-5,nw=RRAM_SIZE-6;
+		fr->lineset(z);
+		fr->lineop(z,yfore,back);
+		fr->lineset(w);
+		fr->lineop(w,fore,yback);
+		fr->lineset(nz);
+		fr->lineop(nz,z);
+		fr->lineset(nw);
+		fr->lineop(nw,w);
+		fr->lineset(fore);
+		fr->lineop(fore,nz,nw);
+		fr->lineset(back);
+		fr->lineop(back,fore);
+		//z=1
+		//z&=!y
+		//z&=x
+		//w=1
+		//w&=!x
+		//w&=y
+		//nz=1
+		//nz&=!z
+		//nw=1
+		//nw&=!w
+		//x=1
+		//x&=!nz
+		//x&=!nw
+		//nx=1
+		//nx&=x
+	}
+};
